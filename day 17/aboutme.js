7c84d612d35cf61a7f7fde5ece3cfe8de98aa77a
aswithaa2011 <160911533+aswithaa2011@users.noreply.github.com>
2025-11-30 13:31:26 +0530

    Assignment day 17

---
+++ day 17/aboutme.js
@@ -0,0 +1,9 @@
+
+    const favoriteHobby = "Reading";
+    const favoriteFood = "Biriyani";
+    const favoriteColor = "Pink";
+
+    console.log("About me")
+    console.log("My favorite hobby is:", favoriteHobby);
+    console.log("My favorite food is:", favoriteFood);
+    console.log("My favorite color is:", favoriteColor);
---
+++ day 17/practicetask.js
@@ -0,0 +1,20 @@
+//name
+let name="Aswitha"
+console.log(name)
+
+//storing age
+
+var myage=23
+
+console.log(myage)
+
+//function   
+
+function gm(){
+console.log("GOOD MORNING")
+}
+
+gm()
+
+
+// Javascript is a high-level interpreted programming language used to create interactive web pages
