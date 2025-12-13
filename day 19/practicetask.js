d05ce818da48686d49fda2497c49139f0607bfb8
aswithaa2011 <160911533+aswithaa2011@users.noreply.github.com>
2025-12-13 12:47:06 +0530

    Assignment day 19

---
+++ day 19/practicetask.js
@@ -0,0 +1,43 @@
+//  let num=10
+
+//  let str="ASWITHA"
+
+//  let condition=true
+
+//  let A=undefined
+
+//  let data=null
+
+//  let id=Symbol("id")
+
+
+//  let bnum=111111111111111111111111111999n;
+
+//  console.log(typeof(num));
+// console.log(typeof(str));
+//  console.log(typeof(condition));
+//  console.log(typeof(A));
+//  console.log(typeof(data));
+//  console.log(typeof(id));
+//  console.log(typeof(bnum));
+
+//Create an object with 3 properties and print them.
+
+ // objects
+
+
+//  const student={
+//     name:"ASWITHA",
+//     age:"23",
+//     Course:"FS"
+//  }
+//  console.log(student);
+ 
+//Convert "45" to a number.
+
+let num=Number("45")
+
+console.log(num);
+
+
+
