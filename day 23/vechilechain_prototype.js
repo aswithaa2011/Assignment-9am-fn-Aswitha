274d95cedb0012f2d1807e795cf1da9d753a6fb4
aswithaa2011 <160911533+aswithaa2011@users.noreply.github.com>
2025-12-21 21:13:05 +0530

    Assignment day 23

--- day 23/vechilechain_prototype.js
+++ day 23/vechilechain_prototype.js
@@ -1,5 +1,18 @@
+function Vehicle(name) {
+  this.name = name;
+}
 
-class vechile{}
+Vehicle.prototype.start = function () {
+  console.log(`${this.name} started`);
+};
 
+Vehicle.prototype.charge = function () {
+  console.log(`${this.name} is charging`);
+};
 
-    
+const car = new Vehicle("Car");
+const electricCar = new Vehicle("Electric Car");
+
+car.start();        
+electricCar.start(); 
+electricCar.charge();
